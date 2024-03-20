ac47ec64f8ddcd5c7f91487a2bdcafad9d982d75
QuaRang <dbduddnd1225@gmail.com>
2024-03-20 23:53:17 +0900

    Initial Commit

---
+++ PokeDex_Developer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PokeDex_Developer
+//
+//  Created by 유영웅 on 3/20/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PokeDex_Developer/PokeDex_DeveloperApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeDex_DeveloperApp.swift
+//  PokeDex_Developer
+//
+//  Created by 유영웅 on 3/20/24.
+//
+
+import SwiftUI
+
+@main
+struct PokeDex_DeveloperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
