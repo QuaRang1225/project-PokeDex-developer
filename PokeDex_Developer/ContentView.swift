25518ecd12744906e584e0885f2c44e048f31887
QuaRang <dbduddnd1225@gmail.com>
2024-03-26 16:15:52 +0900

    #8 - 개발자 로그인 기능 구현
    - 개발자 로그인 화면 구현
    - 로그인 및 회원가입 메서드 싱글톤으로 구현
    - 싱글턴 메서드를 사용하는 viewmodel 생성
    - 유저정보를 저장할 DTO 구조체 생성
    - 로그인이 된 유저는 로그인 없이 바로 실행하도록 구현
    - 로그아웃 기능 구현

--- PokeDex_Developer/ContentView.swift
+++ PokeDex_Developer/ContentView.swift
@@ -9,23 +9,66 @@ import SwiftUI
 
 
 struct ContentView: View {
+    @State private var showSignInView = false
     
     var body: some View {
+        
+        
         ScrollView{
-//            VStack(alignment: .leading){
-//                HStack{
-//                    Image(systemName: "hare.fill")
-//                    Text("포켓몬정보")
-//                        .bold()
-//                }
-//                .foregroundStyle(.primary)
-//                .font(.title2)
-//                
-//            }
+            LazyVStack(pinnedViews: .sectionHeaders){
+                Section(header:header){
+                    VStack(alignment: .leading){
+                        HStack{
+                            Image(systemName: "hare.fill")
+                            Text("포켓몬정보")
+                                .bold()
+                        }
+                        .foregroundStyle(.primary)
+                        .font(.title2)
+                        
+                    }
+                }
+            }
+            
+        }
+        .onAppear{
+            let authUser = try? AuthManager.shared.getUser()  //오류메세지를 확인할 필요가 없으니까 굳이 do catch로 나눌 필요가 앖음
+            self.showSignInView = authUser == nil ? true:false  //유저 정보가 저장되어있을 경우 세
+        }
+        .fullScreenCover(isPresented: $showSignInView) {
+            NavigationStack{
+                SignEmailView(showSignView: $showSignInView)
+            }
+            
         }
     }
 }
 
 #Preview {
     ContentView()
+}
+
+extension ContentView{
+    var header:some View{
+        HStack(alignment: .bottom){
+            Text("포켓몬DB 대시보드")
+                .bold()
+                .font(.title)
+            Spacer()
+            Button {
+                do{
+                    try AuthManager.shared.logout()
+                    showSignInView = true
+                }
+                catch{
+                    print(error)
+                }
+                
+            } label: {
+                Text("로그아웃")
+            }
+            
+        }
+        .padding(.horizontal)
+    }
 }
