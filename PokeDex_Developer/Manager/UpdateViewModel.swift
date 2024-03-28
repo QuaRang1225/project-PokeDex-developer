26926aa8af0fcaa5a4b7c5f98871a1ec7b1f6ad9
QuaRang <dbduddnd1225@gmail.com>
2024-03-28 20:29:08 +0900

    #7 - 코드 간결화 작업

--- PokeDex_Developer/Manager/UpdateViewModel.swift
+++ PokeDex_Developer/Manager/UpdateViewModel.swift
@@ -44,43 +44,43 @@ class UpdateViewModel:ObservableObject{
     
     
     func getPokemonSpecies(num: Int) async throws -> [String: Any] {
-        async let dexNumbers = self.pokemonSpeciesManager.getPokdexNumbers(num: num)
-        async let name =  self.pokemonSpeciesManager.getName(num: num)
-        async let genra =  self.pokemonSpeciesManager.getGenra(num: num)
-        async let eggGroups =  self.pokemonSpeciesManager.getEggGroups(num: num)
-        async let genderRate =  self.pokemonSpeciesManager.getGenderRate(num: num)
-        async let captureRate = self.pokemonSpeciesManager.getCaptureRate(num: num)
-        async let hatchCounter =  self.pokemonSpeciesManager.getHatchCounter(num: num)
-        async let textEntries =  self.pokemonSpeciesManager.getTextEntries(num: num)
-        async let formsSwitchable =  self.pokemonSpeciesManager.getFormsSwitchable(num: num)
-        async let evolutionChainUrl =  self.pokemonEvolutionManager.getEvolutionChainUrl(num: num)
+        async let dexNumbers = pokemonSpeciesManager.getPokdexNumbers(num: num)
+        async let name = pokemonSpeciesManager.getName(num: num)
+        async let genra = pokemonSpeciesManager.getGenra(num: num)
+        async let eggGroups = pokemonSpeciesManager.getEggGroups(num: num)
+        async let genderRate = pokemonSpeciesManager.getGenderRate(num: num)
+        async let captureRate = pokemonSpeciesManager.getCaptureRate(num: num)
+        async let hatchCounter = pokemonSpeciesManager.getHatchCounter(num: num)
+        async let textEntries = pokemonSpeciesManager.getTextEntries(num: num)
+        async let formsSwitchable = pokemonSpeciesManager.getFormsSwitchable(num: num)
+        async let evolutionChainUrl = pokemonEvolutionManager.getEvolutionChainUrl(num: num)
         
         
-        return await [
+        return try await [
             "num" : num,
-            "dex_region": try dexNumbers.0,
-            "dex_num": try dexNumbers.1,
-            "name": try name,
-            "genra": try genra,
-            "egg_group": try eggGroups,
-            "gender_rate": try genderRate,
-            "capture_rate": try captureRate,
-            "hatch_counter": try hatchCounter,
-            "text_entries_version": try textEntries.0,
-            "text_entries_text": try textEntries.1,
-            "forms_switchable": try formsSwitchable,
-            "evolution_tree": try evolutionChainUrl
+            "dex_region": dexNumbers.0,
+            "dex_num": dexNumbers.1,
+            "name": name,
+            "genra": genra,
+            "egg_group":  eggGroups,
+            "gender_rate": genderRate,
+            "capture_rate": captureRate,
+            "hatch_counter": hatchCounter,
+            "text_entries_version": textEntries.0,
+            "text_entries_text": textEntries.1,
+            "forms_switchable": formsSwitchable,
+            "evolution_tree": evolutionChainUrl
         ] as [String : Any]
     }
     func getPokemon(form:String,num:Int)async throws -> [String:Any]{
   
         async let formsName = pokemonManager.getFormsName(name: form)
-        async let formsImages = try await pokemonManager.getFormsImage(name: form, getOnlyForms: false)
-        async let types = try await pokemonManager.getTypes(name: form)
-        async let height = try await pokemonManager.getHeight(name: form)
-        async let weight = try await pokemonManager.getWeight(name: form)
-        async let abilites = try await pokemonManager.getAbilites(name: form)
-        async let stats = try await pokemonManager.getStats(name: form)
+        async let formsImages = pokemonManager.getFormsImage(name: form, getOnlyForms: false)
+        async let types = pokemonManager.getTypes(name: form)
+        async let height = pokemonManager.getHeight(name: form)
+        async let weight = pokemonManager.getWeight(name: form)
+        async let abilites = pokemonManager.getAbilites(name: form)
+        async let stats = pokemonManager.getStats(name: form)
         
         return try await[
             "forms_name" : formsName,
