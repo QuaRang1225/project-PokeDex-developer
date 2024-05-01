e76a376239383f3556833d005738824e0f9d3d44
QuaRang <dbduddnd1225@gmail.com>
2024-05-01 19:49:14 +0900

    포켓몬 DB 대시보드 관련 - 기존엔 포켓몬 정보저장이 특정 도감 번호 범위별 업데이트만 존재했으나 하나씩 저장할 수 있도록 수정 - UI 코드 모듈화 - 모바일앱이 아닌 맥앱으로 관리할 예정

--- PokeDex_Developer/View/PokemonInfoView.swift
+++ PokeDex_Developer/View/PokemonInfoView.swift
@@ -6,69 +6,96 @@
 //
 
 import SwiftUI
+import Kingfisher
 
 struct PokemonInfoView: View {
     @StateObject var vm = UpdateViewModel()
     @State var firstNum = ""
     @State var lastNum = ""
-    
+    @State var num = ""
     
     var body: some View {
-        VStack(alignment: .leading){
-            HStack(alignment: .bottom){
-                Image(systemName: "hare.fill")
-                Text("포켓몬정보")
-                    .bold()
-                Spacer()
-            }
-            .font(.title2)
-            .padding(.top)
-            HStack(alignment: .bottom){
-                Group{
-                    TextField("시작번호",text: $firstNum)
-                        .keyboardType(.numberPad)
-                    Text("~")
-                    TextField("끝번호",text: $lastNum)
-                        .keyboardType(.numberPad)
-                }
-                .frame(width: 60)
-                .font(.body)
-                Spacer()
-                Button{
-                    Task{
-                        await withThrowingTaskGroup(of: Void.self) { group in
-                            if let first = Int(firstNum),let last = Int(lastNum){
-                                for i in first...last{
-                                    group.addTask {
-                                        try await vm.updatePokemonInfo(num: i)
-                                    }
-                                }
-                            }
-                        }
-                    }
-                } label: {
-                    Text("업데이트")
-                        .padding(5)
-                        .padding(.horizontal)
-                        .background(.pink)
-                        .cornerRadius(10)
-                        .foregroundColor(.white)
-                        .bold()
-                }
-               
-            }
+        VStack{
+            title(text: "포켓몬 정보", imageLink: "https://github.com/PokeAPI/sprites/blob/master/sprites/items/poke-ball.png?raw=true")
+            storePokemons
+            storePokemon
         }
         .foregroundStyle(.primary)
         .padding(.horizontal)
-    }
-    
-    func pokemonDataUpdate(){
-        Task{
-            
-        }
     }
 }
 
 #Preview {
     PokemonInfoView()
+}
+
+extension PokemonInfoView{
+    func title(text:String,imageLink:String) -> some View{
+        HStack{
+            KFImage(URL(string: imageLink))
+                .resizable()
+                .frame(width: 50,height: 50)
+            Text(text)
+                .bold()
+            Spacer()
+        }
+        .font(.title2)
+        .padding(.top)
+    }
+    func updateButton(type:String,action:@escaping()->()) -> some View{
+        Button(action: action) {
+            Text(type)
+                .padding(5)
+                .padding(.horizontal)
+                .background(.pink)
+                .cornerRadius(10)
+                .foregroundColor(.white)
+                .bold()
+        }
+    }
+    var storePokemons: some View{
+        VStack(alignment: .leading){
+            HStack(alignment: .bottom){
+                Group{
+                    TextField("시작번호",text: $firstNum)
+                    Text("~")
+                    TextField("끝번호",text: $lastNum)
+                }
+                .frame(width: 60)
+                .font(.body)
+                Spacer()
+                
+                updateButton(type: "저장"){
+                    Task{
+                        await withThrowingTaskGroup(of: Void.self) { group in
+                            guard let first = Int(firstNum),let last = Int(lastNum) else { return }
+                            for i in first...last{
+                                group.addTask {
+                                    try await vm.updatePokemonInfo(num: i)
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    var storePokemon:some View{
+        VStack(alignment: .leading){
+            
+            HStack(alignment: .bottom){
+                TextField("번호",text: $firstNum)
+                    .frame(width: 60)
+                    .font(.body)
+                Spacer()
+                updateButton(type: "저장"){
+                    Task{
+                        guard let num = Int(num) else {return}
+                        try await vm.storePokemon(num: num)
+                    }
+                }
+            }
+        }
+    }
+    
 }
