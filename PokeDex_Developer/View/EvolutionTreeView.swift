5a28d4dffd11d2b23774d584b4f8a251c646681b
QuaRang <dbduddnd1225@gmail.com>
2024-05-08 11:51:57 +0900

    #13 - 포켓몬 진화트리 DTO 구현
    - 인코딩 디코딩을 위한 DTO 구현

---
+++ PokeDex_Developer/View/EvolutionTreeView.swift
@@ -0,0 +1,18 @@
+//
+//  EvolutionTreeView.swift
+//  PokeDex_Developer
+//
+//  Created by 유영웅 on 5/7/24.
+//
+
+import SwiftUI
+
+struct EvolutionTreeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EvolutionTreeView()
+}
